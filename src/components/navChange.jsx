d3338d53e5f997efd13712a3147ecabd70b23811
chukwuebuka Anyadiegwu <chubukass@gmail.com>
2022-08-21 18:09:05 +0100

    create logic to chnage header colors

--- src/components/navChange.jsx
+++ src/components/navChange.jsx
@@ -1,14 +1,31 @@
+import { useAppContext } from "../context";
+import { ACTIVE } from "../context/types";
+
+const hearderChange = ["Personal Info", "Billing Info", "Confirm Payment"];
+
 const NavChange = () => {
+  const [appState, dispatch] = useAppContext();
+
+  const { active } = appState;
+
+  const mappedHeaders = hearderChange.map((data, i) => (
+    <div
+      key={i}
+      className="cursor-pointer relative"
+      onClick={() => dispatch({ type: ACTIVE, payload: data })}
+    >
+      <p className={`text-xs ${active === data ? "text-[#F2994A]" : ""}`}>
+        {data}
+      </p>
+      {active === data && (
+        <p className="border-b-8 border-[#F2994A] rounded-lg absolute -bottom-4 w-full"></p>
+      )}
+    </div>
+  ));
+
   return (
     <div className="border-b border-black text-sm md:text-base md:font-semibold text-[#BDBDBD]">
-      <div className="flex justify-between pb-3">
-        <div className="cursor-pointer relative">
-          <p>Personal Info</p>
-          <p className="border-b-8 border-[#F2994A] rounded-lg absolute -bottom-4 w-full"></p>
-        </div>
-        <div className="mx-12 cursor-pointer">Billing Info</div>
-        <div className="cursor-pointer">Comfirm Payment</div>
-      </div>
+      <div className="flex justify-between pb-3">{mappedHeaders}</div>
     </div>
   );
 };
