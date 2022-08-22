7c2900c0a74a7497b1b6d10d4b7d4abc78c0b008
chukwuebuka Anyadiegwu <chubukass@gmail.com>
2022-08-22 13:44:35 +0100

    create a logic to switch btw component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
 import Background from "./components/background";
 import Container from "./components/container";
+import PurchaseCompleted from "./components/purchaseCompleted";
+import { useAppContext } from "./context";
 
 function App() {
+  const [appState] = useAppContext();
+
+  const { completed } = appState;
+
   return (
     <Background>
-      <Container />
+      {!completed ? <Container /> : <PurchaseCompleted />}
     </Background>
   );
 }
